4c3386daced56b419662cb20c4913475ccfd0283
Samuel Colvin <s@muelcolvin.com>
2020-09-29 17:21:46 +0100

    fix request data in errors

--- foxglove/middleware.py
+++ foxglove/middleware.py
@@ -147,6 +147,8 @@ async def request_log_extra(
             response_body=lenient_json(await get_response_body(response)),
         )
 
+    extra['request_data'] = request_data = lenient_json(request.scope.get('_body'))
+
     return dict(
         extra=extra,
         user=dict(ip_address=get_ip(request)),
@@ -157,7 +159,7 @@ async def request_log_extra(
             cookies=dict(request.cookies),
             headers=dict(request.headers),
             method=request.method,
-            data=lenient_json(request.scope.get('_body')),
+            data=request_data,
             inferred_content_type=request.headers.get('Content-Type'),
         ),
     )
