a745f549984cb1f8c44735e0aa35dd1485d4bbc1
Samuel Colvin <s@muelcolvin.com>
2020-10-13 19:00:33 +0100

    improved pg middleware

--- foxglove/db/middleware.py
+++ foxglove/db/middleware.py
@@ -1,29 +1,53 @@
-from typing import Callable
+from typing import TYPE_CHECKING
 
-from buildpg.asyncpg import BuildPgConnection
 from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.requests import Request
 
 __all__ = 'PgMiddleware', 'get_db'
 
+if TYPE_CHECKING:
+    from buildpg.asyncpg import BuildPgConnection
+    from starlette.requests import Request
+    from starlette.responses import Response
+    from ..middleware import CallNext
+
+
+class GetPgConn:
+    __slots__ = '_glove', '_conn'
+
+    def __init__(self, glove):
+        self._glove = glove
+        self._conn = None
+
+    async def _get_conn(self) -> 'BuildPgConnection':
+        if self._conn is None:
+            self._conn = await self._glove.pg.acquire()
+        return self._conn
+
+    def __await__(self):
+        return self._get_conn().__await__()
+
+    async def release(self):
+        if self._conn is not None:
+            conn = self._conn
+            self._conn = None
+            await self._glove.pg.release(conn)
+
 
 class PgMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app, check: Callable[[Request], bool] = None):
+    def __init__(self, app):
         super().__init__(app)
-        self.check = check
 
         from ..main import glove
 
         self.glove = glove
 
-    async def dispatch(self, request: Request, call_next):
-        if self.check and not self.check(request):
+    async def dispatch(self, request: 'Request', call_next: 'CallNext') -> 'Response':
+        request.state.get_pg_conn = GetPgConn(self.glove)
+        try:
             return await call_next(request)
-        else:
-            async with self.glove.pg.acquire() as conn:
-                request.state.conn = conn
-                return await call_next(request)
+        finally:
+            await request.state.get_pg_conn.release()
 
 
-def get_db(request: Request) -> BuildPgConnection:
-    return request.state.conn
+async def get_db(request: 'Request') -> 'BuildPgConnection':
+    return await request.state.get_pg_conn()
